2f5ea946d2b73fd62065f21063dcc1619a4f8e71
bakoyannis <83982517+bakoyannis@users.noreply.github.com>
2025-03-19 15:52:51 +0200

    Added Icon

--- source/gui.py
+++ source/gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import PhotoImage, filedialog
 from tkinter import ttk
 import sv_ttk
 
@@ -6,33 +7,62 @@ class Application(tk.Tk):
     def __init__(self):
         super().__init__()
 
+        icon = PhotoImage(file="assets/dit_500.png")
+        self.iconphoto(False, icon)
+
         self.title("Title?")
         self.geometry("700x400")
 
-        self.rowconfigure(0, weight=0)
-        self.rowconfigure(1, weight=0)
-
-        self.columnconfigure(0, weight=0)
-        self.columnconfigure(1, weight=0)
-
         self.tab_bar()
         self.top_bar()
-        
-    def top_bar(self):
-        self.open_file_button = ttk.Button(self, text="Open file...", command=lambda:print("open file pressed"))
-        self.open_file_button.grid(row=1, column=0, sticky="nswe", padx=3, pady=5)
 
-        self.open_db_button = ttk.Button(self, text="Open DB...", command=lambda:print("open db pressed"))
-        self.open_db_button.grid(row=1, column=1, sticky="nswe", padx=3, pady=5)
+    def onOpen(self):
+        ftypes = [('ARF data files', '*.arff'), ('CSV data files', '*.csv'), ('All files', '*')]
+        dlg = filedialog.Open(self, filetypes = ftypes)
+        fl = dlg.show()
+
+        if fl:
+            text = self.readFile(fl)
+            print(text)
+
+    def readFile(self, filename):
+        f = open(filename, "r")
+        text = f.read()
+        return text
+
+    def top_bar(self):
+        self.open_file_button = ttk.Button(self.preprocess_tab, text="Open file...", command=self.onOpen)
+        self.open_file_button.grid(row=0, column=0, sticky="nswe", padx=3, pady=5)
+
+        self.open_db_button = ttk.Button(self.preprocess_tab, text="Open DB...", command=lambda:print("open db pressed"))
+        self.open_db_button.grid(row=0, column=1, sticky="nswe", padx=3, pady=5)
 
     def tab_bar(self):
         tab_bar_obj = ttk.Notebook(self)
-        tab1 = ttk.Frame(tab_bar_obj)
-        tab2 = ttk.Frame(tab_bar_obj)
 
-        tab_bar_obj.add(tab1, text ='Preprocess') 
-        tab_bar_obj.add(tab2, text ='Classify')
-        tab_bar_obj.grid(row=0, column=0, columnspan=2, sticky="w")
+        # Create tabs
+        self.preprocess_tab = ttk.Frame(tab_bar_obj)
+        self.classify_tab = ttk.Frame(tab_bar_obj)
+
+        # Add tabs to the notebook
+        tab_bar_obj.add(self.preprocess_tab, text='Preprocess')
+        tab_bar_obj.add(self.classify_tab, text='Classify')
+
+        # Add the notebook to the grid and make it expand
+        tab_bar_obj.grid(row=0, column=0, sticky="nswe")
+
+        # Configure the grid weights for the notebook
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.preprocess_tab.grid_rowconfigure(0, weight=0)
+        self.preprocess_tab.grid_rowconfigure(1, weight=1)
+        self.preprocess_tab.grid_columnconfigure(0, weight=0)
+        self.preprocess_tab.grid_columnconfigure(1, weight=0)
+        self.preprocess_tab.grid_columnconfigure(2, weight=2)
+
+        self.classify_tab.grid_rowconfigure(0, weight=1)
+        self.classify_tab.grid_columnconfigure(0, weight=1)
 
 
 if __name__ == "__main__":
